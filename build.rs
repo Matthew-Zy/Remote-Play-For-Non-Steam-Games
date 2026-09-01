f841e240063ed359a2c82d82113e295a2c2be742
Matthew-Zy <166348388+Matthew-Zy@users.noreply.github.com>
2026-08-31 20:39:48 -0700

    slint setup

--- build.rs
+++ build.rs
@@ -1,9 +1,11 @@
-#[cfg(windows)]
-fn main() {
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("assets/icon.ico");
-    res.compile().unwrap();
-}
 
-#[cfg(not(windows))]
-fn main() {}
+
+fn main() {
+    slint_build::compile("gui/appwindow.slint").expect("Slint build failed");
+    #[cfg(windows)]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("assets/icon.ico");
+        res.compile().unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,13 @@ mod game_loader;
 mod tui;
 mod cli; 
 
+slint::include_modules!();
+use slint::{SharedString, ToSharedString};
+
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     let use_cli = args.contains(&String::from("--cli"));
+    let use_gui = args.contains(&String::from("--gui"));
     for x in args {
         if let Some(custom_conf) = x.strip_prefix("-conf=") {
             game_loader::set_custom_conf_path(custom_conf.to_string());
@@ -13,7 +17,24 @@ fn main() {
     }
     if use_cli {
         cli::run_cli();
-    } else {
+    } else if use_gui {
+        run_slint_gui();
+    }
+    else {
         let _ = tui::run_tui();
     }
+}
+
+fn test() -> SharedString {
+    return "skibidi".to_shared_string();
+}
+
+fn run_slint_gui() {
+    
+    let gui = AppWindow::new().unwrap();
+
+    gui.on_test_function(test);
+
+
+    gui.run().unwrap();
 }
